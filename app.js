e890767860a8725db43a6bd6cf2f6c8a3e85badf
xyf0921 <974708644@qq.com>
2018-06-06 17:57:40 +0800

    t commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Good afternoon!!!!')
+  res.send('Good afternoon!')
 })
 
 app.listen(3000, function () {
